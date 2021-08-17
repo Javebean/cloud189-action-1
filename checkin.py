43f87aef08c04fcc3452667b6f8c0dea837652f0
Javebean <448267696@qq.com>
2021-08-17 09:55:01 +0800

    Update checkin.py

--- checkin.py
+++ checkin.py
@@ -138,7 +138,7 @@ def calculate_md5_sign(params):
 
 def login(username, password):
     url = "https://cloud.189.cn/udb/udb_login.jsp?pageId=1&redirectURL=/main.action"
-    r = s.get(url)r
+    r = s.get(url)
     print(f"--------geturl...{r}")
     captchaToken = re.findall(r"captchaToken' value='(.+?)'", r.text)[0]
     lt = re.findall(r'lt = "(.+?)"', r.text)[0]
